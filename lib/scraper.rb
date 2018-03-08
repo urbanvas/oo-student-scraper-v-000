9bde696771a9f46b94407eed6b28153497f5133e
Neil Seejoor <nvseejoor@gmail.com>
2018-03-08 17:44:20 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
-    doc.css('roster-cards-container').each do |student|
+    doc.css('.roster-cards-container').each do |student|
       # binding.pry
     end
     binding.pry
